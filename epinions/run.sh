2bea3eadf2f53fca9e3748573bf886584b0b41e4
Eriq Augustine <eriq.augustine@gmail.com>
2017-11-30 19:54:59 -0800

    Added special MLN query generation for epinions.

--- epinions/run.sh
+++ epinions/run.sh
@@ -6,6 +6,10 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && source "${THIS_D
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && source "${THIS_DIR}/../scripts/tuffy.sh"
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+QUERY_FILE="${THIS_DIR}/mln/query.db"
+BASE_QUERY_FILE="${THIS_DIR}/mln/query-base.db"
+GEN_QUERY_SCRIPT="${THIS_DIR}/scripts/generateMLNQuery.rb"
+
 function run() {
    local outBaseDir="${THIS_DIR}/out"
    local folds=`seq -s ' ' 0 7`
@@ -21,6 +25,9 @@ function run() {
          '-ec' \
          true
 
+      # Weight learning needs a modified query that contians all the targets.
+      ruby "${GEN_QUERY_SCRIPT}" "${THIS_DIR}/data/splits/${fold}/learn" "${QUERY_FILE}"
+
       # Tuffy
       tuffy::runLearn \
          "${outBaseDir}/tuffy/${fold}" \
@@ -28,12 +35,17 @@ function run() {
          "${THIS_DIR}/scripts" \
          "${THIS_DIR}/data/splits/${fold}/learn"
 
+      # Evaluation can use the default query.
+      cp "${BASE_QUERY_FILE}" "${QUERY_FILE}"
+
       tuffy::runEval \
          "${outBaseDir}/tuffy/${fold}" \
          "${THIS_DIR}/mln" \
          "${THIS_DIR}/scripts" \
          "${THIS_DIR}/data/splits/${fold}/eval" \
          "${outBaseDir}/tuffy/${fold}/${LEARNED_MLN_MODEL_FILENAME}"
+
+      rm "${QUERY_FILE}"
    done
 }
 
