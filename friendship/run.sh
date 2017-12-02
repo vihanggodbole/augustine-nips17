3589eb7c916de70a7b93bfcc8c3f7a2586080af9
Eriq Augustine <eriq.augustine@gmail.com>
2017-12-02 08:54:45 -0800

    Added more runs for the friendship experiment.

--- friendship/run.sh
+++ friendship/run.sh
@@ -7,11 +7,6 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DATA_GEN_SCRIPT="${THIS_DIR}/scripts/generateFriendshipData.rb"
 
-# Redefine for experiment specifics.
-PSL_METHODS=('psl-admm-postgres' 'psl-mosek-postgres' 'psl-cvxpy-postgres')
-PSL_METHODS_CLI_OPTIONS=('--postgres psl' "`psl::mosekOptions` --postgres psl" "`psl::cvxpxOptions` --postgres psl")
-PSL_METHODS_JARS=("${PSL_JAR_PATH}" "${PSL_JAR_PATH}:${PSL_MOSEK_JAR_PATH}" "${PSL_JAR_PATH}:${PSL_CVXPY_JAR_PATH}")
-
 # Limit to 300G
 ulimit -d 314572800
 
@@ -20,8 +15,53 @@ ulimit -d 314572800
 
 function run() {
    local outBaseDir="${THIS_DIR}/out"
-   local folds="$(seq -w -s ' ' 30 10 0100)"
-   # local folds="$(seq -w -s ' ' 30 10 0100) $(seq -w -s ' ' 100 100 1000)"
+
+   local folds="$(seq -w -s ' ' 30 10 0200)"
+   PSL_METHODS=('psl-admm-postgres' 'psl-mosek-postgres' 'psl-cvxpy-postgres')
+   PSL_METHODS_CLI_OPTIONS=('--postgres psl' "`psl::mosekOptions` --postgres psl" "`psl::cvxpxOptions` --postgres psl")
+   PSL_METHODS_JARS=("${PSL_JAR_PATH}" "${PSL_JAR_PATH}:${PSL_MOSEK_JAR_PATH}" "${PSL_JAR_PATH}:${PSL_CVXPY_JAR_PATH}")
+
+   for fold in $folds; do
+      # Generate the data.
+      ruby $DATA_GEN_SCRIPT -p $fold -l 10 -fh 0.85 -fl 0.15 -n friendship
+      local dataDir="${THIS_DIR}/data/friendship_${fold}_0010"
+
+      # PSL
+      psl::runSuite \
+         'friendship' \
+         "${THIS_DIR}" \
+         "${fold}" \
+         '' \
+         "${fold}" \
+         '-ec -ed 0.5' \
+         false
+   done
+
+   local folds="$(seq -w -s ' ' 100 100 0600)"
+   PSL_METHODS=('psl-admm-postgres' 'psl-mosek-postgres')
+   PSL_METHODS_CLI_OPTIONS=('--postgres psl' "`psl::mosekOptions` --postgres psl")
+   PSL_METHODS_JARS=("${PSL_JAR_PATH}" "${PSL_JAR_PATH}:${PSL_MOSEK_JAR_PATH}")
+
+   for fold in $folds; do
+      # Generate the data.
+      ruby $DATA_GEN_SCRIPT -p $fold -l 10 -fh 0.85 -fl 0.15 -n friendship
+      local dataDir="${THIS_DIR}/data/friendship_${fold}_0010"
+
+      # PSL
+      psl::runSuite \
+         'friendship' \
+         "${THIS_DIR}" \
+         "${fold}" \
+         '' \
+         "${fold}" \
+         '-ec -ed 0.5' \
+         false
+   done
+
+   local folds="$(seq -w -s ' ' 100 100 1000)"
+   PSL_METHODS=('psl-admm-postgres')
+   PSL_METHODS_CLI_OPTIONS=('--postgres psl')
+   PSL_METHODS_JARS=("${PSL_JAR_PATH}")
 
    for fold in $folds; do
       # Generate the data.
